520a726e39ca2ff2797833c54ec303c27c0e0266
Lelental <p.lelental@gmail.com>
2020-03-15 20:57:30 +0100

    Initial commit

---
+++ Assets/Scripts/PlayerControllerLevel1.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerLevel1 : MonoBehaviour
+{
+    public float moveSpeed = 2.5f;
+    public float jumpForce = 4f;
+    public Rigidbody2D rigidbody2D;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+}
