4c66951edc112526807fc53ddf9bc43aae3594ca
Lelental <p.lelental@gmail.com>
2020-03-16 21:46:06 +0100

    Added jumping and walking functionality

--- Assets/Scripts/CameraControllerLevel1.cs
+++ Assets/Scripts/CameraControllerLevel1.cs
@@ -5,18 +5,18 @@ using System.Collections;
 public class CameraControllerLevel1 : MonoBehaviour
 {
     public GameObject player;
-    private Vector3 offset;
+    private Vector3 _offset;
 
     void Start()
     {
         //Calculate and store the offset value by getting the distance between the player's position and camera's position.
-        offset = transform.position - player.transform.position;
+        _offset = transform.position - player.transform.position;
     }
 
     // LateUpdate is called after Update each frame
     void LateUpdate()
     {
         // Set the position of the camera's transform to be the same as the player's, but offset by the calculated offset distance.
-        transform.position = player.transform.position + offset;
+        transform.position = player.transform.position + _offset;
     }
 }
--- Assets/Scripts/PlayerControllerLevel1.cs
+++ Assets/Scripts/PlayerControllerLevel1.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerControllerLevel1 : MonoBehaviour
 {
     public float moveSpeed = 2.5f;
-    public float jumpForce = 4f;
-    public Rigidbody2D rigidbody2D;
+    public float jumpForce = 1f;
+    public new Rigidbody2D rigidbody2D;
+    public LayerMask groundLayer;
+    public Animator animator;
+
+    private bool _isWalking;
 
     // Start is called before the first frame update
     void Start()
@@ -16,24 +20,45 @@ public class PlayerControllerLevel1 : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0))
+        {
+            Jump();
+        }
+
+        _isWalking = false;
+
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             transform.Translate(moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            _isWalking = true;
         }
 
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             transform.Translate(-moveSpeed * Time.deltaTime, 0.0f, 0.0f, Space.World);
+            _isWalking = true;
         }
 
-        if (Input.GetMouseButtonDown(0))
-        {
-            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
-        }
+        animator.SetBool("isGrounded", IsGrounded());
+        animator.SetBool("isWalking", _isWalking);
     }
 
     void Awake()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
+        Debug.Log("jumping");
+    }
+
+    public void Jump()
+    {
+        if (IsGrounded())
+        {
+            rigidbody2D.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    public bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.5f, groundLayer.value);
     }
 }
