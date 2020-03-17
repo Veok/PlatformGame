78e64886e855bfac0691da0b1e958d59398b389c
Lelental <p.lelental@gmail.com>
2020-03-17 20:51:56 +0100

    Commited missing coin controller

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinController : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.Play("CoinRotation", 0, Random.Range(0.0f, 1f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
