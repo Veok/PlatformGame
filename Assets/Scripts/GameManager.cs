f5ccd3def3148d2e16e8c06afe4314df681765d9
Lelental <p.lelental@gmail.com>
2020-03-26 18:18:10 +0100

    GameManger

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameState currentGameState;
+
+    public static GameManager instance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PauseMenu();
+    }
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    void SetGameState(GameState newGameState)
+    {
+        currentGameState = newGameState;
+    }
+
+    public void InGame()
+    {
+        SetGameState(GameState.GS_GAME);
+    }
+
+
+    public void GameOver()
+    {
+        SetGameState(GameState.GS_GAME_OVER);
+    }
+
+    public void PauseMenu()
+    {
+        SetGameState(GameState.GS_PAUSEMENU);
+    }
+
+    public void LevelCompleted()
+    {
+        SetGameState(GameState.GS_LEVELCOMPLETED);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (instance.currentGameState == GameState.GS_PAUSEMENU)
+        {
+            if (Input.GetKey(KeyCode.S))
+            {
+                instance.InGame();
+            }
+        }
+    }
+}
+
+public enum GameState
+{
+    GS_PAUSEMENU,
+    GS_GAME,
+    GS_LEVELCOMPLETED,
+    GS_GAME_OVER
+}
