68978f640781169a1c429c8ce4723d7137c37ca9
Lelental <p.lelental@gmail.com>
2020-03-30 19:24:23 +0200

    Added mainmenu script

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    private IEnumerator StartGame(string levelName)
+    {
+        yield return new WaitForSeconds(0.1f);
+        SceneManager.LoadScene(levelName);
+    }
+
+    public void OnLevel1ButtonPressed()
+    {
+        StartCoroutine(StartGame("save"));
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
